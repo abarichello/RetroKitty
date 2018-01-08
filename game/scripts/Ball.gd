610f0431bedf871b9695bbf9b4b790ea9852fd70
aBarichello <arturbarichello@hotmail.com>
2018-01-08 00:22:37 -0200

    Random Speed

--- game/scripts/Ball.gd
+++ game/scripts/Ball.gd
@@ -1,13 +1,14 @@
 extends RigidBody2D
 
-export (int) var speed = 300
+export (int) var min_speed = 125
+export (int) var max_speed = 300
 var angle
 
 func create(start_position, angle):
 	self.global_position = start_position
 	self.angle = angle
-	var direction = get_node("/root/Main/Player").position - global_position
-	apply_impulse(Vector2(), direction)
+	var direction = (get_node("/root/Main/Player").position - global_position).normalized()
+	apply_impulse(Vector2(), direction * rand_range(min_speed, max_speed))
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
