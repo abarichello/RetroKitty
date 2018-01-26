fdfff0d87cdbd2c5122e8e6385662285baafe3dd
aBARICHELLO <arturbarichello@hotmail.com>
2018-01-26 15:16:54 -0200

    Bat Rewrite

--- game/scripts/Bat.gd
+++ game/scripts/Bat.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
+enum Direction {UP = 90, DOWN = 270, LEFT = 180, RIGHT = 0}
 onready var Player = get_node("/root/Game/Main/Player")
-var angle
+onready var Hud = get_node("/root/Game/Main/GameHUD")
+var angle = 0
 var alive = true
+
+func _ready():
+	$Sprite/Hitzone.monitoring = true
 
 func _process(delta):
 	if Player:
@@ -11,20 +16,40 @@ func _process(delta):
 func _input(event):
 	if alive:
 		if event.is_action_pressed("ui_fire"):
+			hit_bodies_in_hitzone()
 			$Cooldown.start()
 		if event.is_action_pressed("ui_up"):
 			$Sprite.rotation = 0
 			$Sprite.flip_v = false
 			$Sprite.position = $Up.position
+			angle = UP
+			self.set_collision_layer_bit(angle, true)
 		if event.is_action_pressed("ui_down"):
 			$Sprite.rotation = 0
 			$Sprite.flip_v = true
 			$Sprite.position = $Down.position
+			angle = DOWN
+			self.set_collision_layer_bit(angle, true)
 		if event.is_action_pressed("ui_left"):
 			$Sprite.rotation = deg2rad(135)
 			$Sprite.flip_v = true
 			$Sprite.position = $Left.position
+			angle = LEFT
+			self.set_collision_layer_bit(angle, true)
 		if event.is_action_pressed("ui_right"):
 			$Sprite.rotation = deg2rad(45)
 			$Sprite.flip_v = false
 			$Sprite.position = $Right.position
+			angle = RIGHT
+			self.set_collision_layer_bit(angle, true)
+
+func hit_bodies_in_hitzone():
+	var bodies = $Sprite/Hitzone.get_overlapping_bodies()
+	for body in bodies:
+		var able = $Cooldown.get_time_left() == 0
+		if (body.angle == angle - 180 || body.angle == angle + 180) && able: # Bat and ball facing eachother
+			body.set_collision_layer_bit(0, false)
+			body.hit = true
+			var direction = body.global_position - Vector2(1, 1)
+			body.linear_velocity = direction * 5
+			Hud.hit_check(body)
--- game/scripts/Player.gd
+++ game/scripts/Player.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 signal game_over
-onready var Hud = get_node("/root/Game/Main/GameHUD")
 enum Direction {UP = 90, DOWN = 270, LEFT = 180, RIGHT = 0}
 
 export (int) var max_hp = 3
@@ -34,13 +33,3 @@ func _on_Player_game_over():
 	print("game over")
 	$Bat.alive = false
 	self.alive = false
-
-func _on_Player_body_entered(body):
-	print(body)
-	var able = $Bat/Cooldown.get_time_left() > 0.1 && $Bat/Cooldown.get_time_left() < 0.3
-	if (body.angle == angle - 180 || body.angle == angle + 180) && able: # Bat and ball facing eachother
-		body.set_collision_layer_bit(0, false)
-		body.hit = true
-		var direction = body.global_position - Vector2(1, 1)
-		body.linear_velocity = direction * 5
-		Hud.hit_check(body)
